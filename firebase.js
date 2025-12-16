21360e08f9083ac37c89e0ee194d0b9baaffda61
Juan-AC <acparceria818@gmail.com>
2025-12-16 16:15:28 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5KEaKntt9wPYcy60DutrqvIH34piXsXk",
