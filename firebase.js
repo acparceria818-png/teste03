e516e9757342ff48a5a41fb02ade63bc6cb4f598
Juan-AC <acparceria818@gmail.com>
2025-12-16 13:59:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5KEaKntt9wPYcy60DutrqvIH34piXsXk",
+  authDomain: "transporte-f7aea.firebaseapp.com",
+  databaseURL: "https://transporte-f7aea-default-rtdb.firebaseio.com",
+  projectId: "transporte-f7aea",
+  storageBucket: "transporte-f7aea.firebasestorage.app",
+  messagingSenderId: "551406731008",
+  appId: "1:551406731008:web:90855ffcd9ac0ef1d93de5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
